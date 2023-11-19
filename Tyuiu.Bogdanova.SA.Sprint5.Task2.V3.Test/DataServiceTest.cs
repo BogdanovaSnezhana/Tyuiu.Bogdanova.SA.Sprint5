293e0201ebe82832fca56cde718f0178db9a8a66
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-11-19 20:54:08 +0500

    Task2

---
+++ Tyuiu.Bogdanova.SA.Sprint5.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.Bogdanova.SA.Sprint5.Task2.V3.Lib;
+using System.IO;
+
+namespace Tyuiu.Bogdanova.SA.Sprint5.Task2.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\User\source\repos\Tyuiu.Bogdanova.SA.Sprint5\Tyuiu.Bogdanova.SA.Sprint5.Task2.V3\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+
+}
+
---
+++ Tyuiu.Bogdanova.SA.Sprint5.Task2.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.Bogdanova.SA.Sprint5.Task2.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.Bogdanova.SA.Sprint5.Task2.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37f606ac-fecb-4f3f-b2c2-811c87420508")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
