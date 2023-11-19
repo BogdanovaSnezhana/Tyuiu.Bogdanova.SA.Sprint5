a4be113cce6b445e8a90758bd49af73bff77a02a
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-11-19 23:33:43 +0500

    Task3

---
+++ Tyuiu.Bogdanova.SA.Sprint5.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.Bogdanova.SA.Sprint5.Task3.V6.Lib;
+using System.IO;
+
+namespace Tyuiu.Bogdanova.SA.Sprint5.Task3.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Bogd\source\repos\Tyuiu.Bogdanova.SA.Sprint5\Tyuiu.Bogdanova.SA.Sprint5.Task3.V6\bin\Debug\OutPutFileTask3.bin";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+    
+}
