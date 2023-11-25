92c3d3d609cd044ecea08e86c5fd1e1c4700077a
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-11-25 19:57:57 +0500

    Task4

---
+++ Tyuiu.Bogdanova.SA.Sprint5.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.Bogdanova.SA.Sprint5.Task4.V19.Lib;
+
+namespace Tyuiu.Bogdanova.SA.Sprint5.Task4.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Test4()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V19.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileex = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileex);
+        }
+
+
+
+    }
+}
---
+++ Tyuiu.Bogdanova.SA.Sprint5.Task4.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.Bogdanova.SA.Sprint5.Task4.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.Bogdanova.SA.Sprint5.Task4.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("073a852b-559f-419f-b1c9-ac10889f8851")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
